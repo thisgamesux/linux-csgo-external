327e7d221298251887dd9508acb5411e2c42ba28
s0beit <s0beit@myg0t.com>
2015-01-13 02:33:12 -0600

    added netvar support, version number increase

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 #include "log.hpp"
 #include "remote.hpp"
 #include "hack.hpp"
+#include "netvar.hpp"
 
 using namespace std;
+
+// This isn't called or used because it's for debugging, use it if you want.
+void printAllClasses() {
+    for(size_t i = 0; i < netvar::GetAllClasses().size(); i++) {
+        cout << netvar::GetAllClasses()[i].name << endl;
+
+        for(size_t p = 0; p < netvar::GetAllClasses()[i].props.size(); p++) {
+            cout << "\t" << netvar::GetAllClasses()[i].props[p].name << " = " << std::hex << netvar::GetAllClasses()[i].props[p].offset << endl;
+        }
+    }
+}
 
 int main() {
     if(getuid() != 0) {
@@ -11,7 +23,7 @@ int main() {
         return 0;
     }
 
-    cout << "s0beit linux hack version 1.2" << endl;
+    cout << "s0beit linux hack version 1.3" << endl;
 
     log::init();
     log::put("Hack loaded...");
@@ -78,12 +90,22 @@ int main() {
 
     if(!csgo.Read((void*) (call + 0x11), &addressOfGlowPointer, sizeof(unsigned long))) {
         cout << "Unable to read address of glow pointer" << endl;
+        return 0;
     }
 
     cout << "Glow Array: " << std::hex << addressOfGlowPointer << endl;
 
     // It's the same!
     // cout << "Glow Address [" << std::hex << addressOfGlowPointer << "] vs [" << (client.start + 0x054612C0) << "]" << endl;
+
+    while(!netvar::Cache(csgo, client)) {
+        if(!csgo.IsRunning()) {
+            cout << "Exited game before netvars could be cached, terminating" << endl;
+            return 0;
+        }
+
+        usleep(5000);
+    }
 
     while(csgo.IsRunning()) {
         hack::Glow(&csgo, &client, addressOfGlowPointer);
