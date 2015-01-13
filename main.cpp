ddef95bb750c709f0d03eefb64ab3a72b2215b01
s0beit <s0beit@myg0t.com>
2015-01-12 22:43:09 -0600

    Added finding patterns + upgrade version!

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main() {
         return 0;
     }
 
-    cout << "s0beit linux hack version 1.1" << endl;
+    cout << "s0beit linux hack version 1.2" << endl;
 
     log::init();
     log::put("Hack loaded...");
@@ -41,7 +41,7 @@ int main() {
         csgo.ParseMaps();
 
         for(auto region : csgo.regions) {
-            if (region.filename.compare("client_client.so") == 0/* && region.executable*/) {
+            if (region.filename.compare("client_client.so") == 0 && region.executable) {
                 cout << "client_client.so: [" << std::hex << region.start << "][" << std::hex << region.end << "][" << region.pathname << "]" << endl;
                 client = region;
                 break;
@@ -53,10 +53,41 @@ int main() {
 
     cout << "Found client_client.so [" << std::hex << client.start << "]" << endl;
 
-    while(csgo.IsRunning()) {
-        hack::Glow(&csgo, &client);
+    //E8 ? ? ? ? 8B 78 14 6B D6 34
+    void* foundGlowPointerCall = client.find(csgo,
+            "\xE8\x00\x00\x00\x00\x8B\x78\x14\x6B\xD6\x34",
+            "x????xxxxxx");
 
-        //sleep(2);
+    if(!foundGlowPointerCall) {
+        cout << "Unable to find glow pointer call reference" << endl;
+        return 0;
+    }
+
+    cout << "Glow function reference: " << std::hex << foundGlowPointerCall << endl;
+
+    unsigned long call = csgo.GetCallAddress(foundGlowPointerCall);
+
+    if(!call) {
+        cout << "Unable to read glow pointer call reference address" << endl;
+        return 0;
+    }
+
+    cout << "Glow function address: " << std::hex << call << endl;
+
+    unsigned long addressOfGlowPointer = 0;
+
+    if(!csgo.Read((void*) (call + 0x11), &addressOfGlowPointer, sizeof(unsigned long))) {
+        cout << "Unable to read address of glow pointer" << endl;
+    }
+
+    cout << "Glow Array: " << std::hex << addressOfGlowPointer << endl;
+
+    // It's the same!
+    // cout << "Glow Address [" << std::hex << addressOfGlowPointer << "] vs [" << (client.start + 0x054612C0) << "]" << endl;
+
+    while(csgo.IsRunning()) {
+        hack::Glow(&csgo, &client, addressOfGlowPointer);
+
         usleep(5000);
     }
 
